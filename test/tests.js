f0a1252d34a5171355ecde8c7bea4de4917a4a98
Ian Storm Taylor <ian@ianstormtaylor.com>
2013-09-18 23:24:08 -0700

    update to-space-case

--- test/tests.js
+++ test/tests.js
@@ -3,28 +3,22 @@ describe('to-camel-case', function () {
 var assert = require('assert');
 var camel = require('to-camel-case');
 
-it('shouldnt touch camel case', function () {
-  assert('aCamelCaseString' == camel('aCamelCaseString'));
-});
+var strings = {
+  camel    : 'thisIsAString',
+  space    : 'this is a string',
+  snake    : 'this_is_a_string',
+  dot      : 'this.is.a.string',
+  title    : 'This Is a String',
+  constant : 'THIS_IS_A_STRING',
+  sentence : 'This is a string.'
+};
 
-it('should convert space case', function () {
-  assert('aSpaceCaseString' == camel('a space case string'));
-});
+function convert (key) {
+  it('should convert ' + key + ' case', function () {
+    assert('thisIsAString' == camel(strings[key]));
+  });
+}
 
-it('should convert snake case', function () {
-  assert('aSnakeCaseString' == camel('a_snake_case_string'));
-});
-
-it('should convert dot case', function () {
-  assert('aDotCaseString' == camel('a.dot.case.string'));
-});
-
-it('should convert title case', function () {
-  assert('aTitleCaseOfString' == camel('A Title: Case of String'));
-});
-
-it('should convert constant case', function () {
-  assert('aConstantCaseString' == camel('A_CONSTANT_CASE_STRING'));
-});
+for (var key in strings) convert(key);
 
 });
